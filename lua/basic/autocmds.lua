68d9b75bda12b8cda19a43dff2ecb3e3facb40d1
line <line@live.com>
2023-01-19 20:57:49 +0800

    fix: 解决打开文件自动跳转上次光标位置偶尔出错的问题

--- lua/basic/autocmds.lua
+++ lua/basic/autocmds.lua
@@ -2,11 +2,20 @@ vim.api.nvim_create_augroup('AuGroup', {
   clear = true,
 })
 
-vim.api.nvim_create_autocmd('BufReadPost', {
+vim.api.nvim_create_autocmd('FileType', {
   pattern = '*',
   group = 'AuGroup',
-  command = 'silent! normal! g`"',
-  desc = '跳转光标'
+  callback = function()
+    if vim.fn.match(vim.bo.filetype, '\\v\\ccommit|rebase') == -1 then
+      local cousor = vim.api.nvim_win_get_cursor(0)
+      local count = vim.api.nvim_buf_line_count(0)
+      if cousor[1] > 0 and cousor[1] < count then
+        vim.cmd('silent! normal! g`"')
+      end
+    end
+  end,
+  once = true,
+  desc = '打开文件自动跳转上次光标位置'
 })
 
 vim.api.nvim_create_autocmd('BufWritePost', {
